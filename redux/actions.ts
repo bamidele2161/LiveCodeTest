36be7f450becd3accec750df092c546d59752025
bamidele2161 <59539644+bamidele2161@users.noreply.github.com>
2022-03-13 17:07:15 +0100

    first commit

---
+++ redux/actions.ts
@@ -0,0 +1,57 @@
+import * as types from "./actionType";
+import axios from 'axios';
+
+const getTasks = (tasks) => ({
+    type: types.GET_TODO,
+    payload: tasks,
+});
+
+const taskDeleted = (id) => ({
+    type: types.DELETE_TODO,
+    payload: id,
+})
+
+const taskAdded = (task) => ({
+    type: types.ADD_TODO,
+    payload: task,
+})
+
+
+export const loadTasks = () => {
+    return function (dispatch) {
+        axios.get(`https://jsonplaceholder.typicode.com/todos`).then((response) => {
+            console.log("response", response);
+            dispatch(getTasks(response.data));
+      })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+};
+    
+export const deleteTask = (id) => {
+    return function (dispatch) {
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then((response) => {
+            console.log("response", response);
+            dispatch(taskDeleted(id));
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    };
+};
+
+export const addTask = (task) => {
+    return function (dispatch) {
+        axios.post(`https://jsonplaceholder.typicode.com/todos`, task)
+        .then((response) => {
+            console.log("response", response);
+            dispatch(taskAdded(response.data));
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+}
---
+++ redux/reducer.ts
@@ -0,0 +1,34 @@
+import * as types from './actionType'
+
+const initialState = {
+    tasks: [],
+    task: {},
+    loading: false
+}
+
+const tasksReducers = (state = initialState, action) => {
+    switch(action.type) {
+        case types.GET_TODO:
+            return { 
+                ...state,
+                tasks: action.payload,
+                loading: false,
+            };
+        case types.DELETE_TODO:
+            return{
+                ...state,
+                loading: false,
+                tasks: state.tasks.filter((item) => item.id !== action.payload),
+            }
+        case types.ADD_TODO:
+            return{
+                ...state,
+                tasks: state.tasks.concat(action.payload),
+                loading: false,
+            }
+        default:
+            return state;
+    }
+};
+
+export default tasksReducers;
